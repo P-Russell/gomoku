fa26624ac15cf18d174b06e3e86ea0a34b2af05f
P-Russell <pdrussell011@gmail.com>
2018-01-09 12:06:09 +0200

    working terminal game

---
+++ src/Board.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class Board extends Capture {
+    private int[][] board = new int[19][19];
+    private int lastPlayed = 2;
+    private int lastX;
+    private int lastY;
+    private boolean gameOver = false;
+    private ValidMove valid = new ValidMove();
+
+    public boolean placeStone(int x, int y){
+
+        if (x < 19 && x >= 0 && y < 19 && y >= 0 && board[y][x] == 0){
+            if (lastPlayed == 1 && !valid.doubleThree(board, y, x,2)) {
+                    board[y][x] = lastPlayed = 2;
+                    capture(board, y, x, lastPlayed);
+                }
+            else if (!valid.doubleThree(board, y, x, 1)) {
+                    capture(board, y, x, 1);
+                    board[y][x] = lastPlayed = 1;
+                    ;
+                }
+            else
+                return false;
+            if (capture(board, y, x, lastPlayed))
+                System.out.println("Capture");
+            lastX = x;
+            lastY = y;
+            return true;
+        }
+        System.out.println("Invalid move");
+        return false;
+    }
+
+    public int getLastPlayed() {
+        return lastPlayed;
+    }
+
+    public int getLastX() {
+        return lastX;
+    }
+
+    public int getLastY() {
+        return lastY;
+    }
+
+    public boolean getGameOver(){
+        if (this.gameOver)
+            return this.gameOver;
+        this.gameOver = valid.isWinMove(this);
+        return this.gameOver;
+    }
+
+    public void setGameOver(boolean bool){
+        this.gameOver = bool;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void printBoard(){
+        for(int i = 0; i < 19; i++)
+            System.out.println(Arrays.toString(board[i]));
+    }
+}
---
+++ src/Capture.java
@@ -0,0 +1,53 @@
+public class Capture {
+    public Boolean capture(int[][] arr, int r, int c, int move){
+        //down
+        if (r < 16)
+            if (move == arr[r + 3][c] && arr[r + 1][c] != move && arr[r + 1][c] == arr[r + 2][c] & arr[r + 1][c] != 0) {
+                arr[r + 1][c] = arr[r + 2][c] = 0;
+                return (true);
+            }
+        //up
+        if (r > 3)
+            if (move == arr[r - 3][c] && arr[r - 1][c] != move && arr[r - 1][c] == arr[r - 2][c] & arr[r - 1][c] != 0){
+                arr[r - 1][c] = arr[r - 2][c] = 0;
+                return (true);
+            }
+        //right
+        if (c < 16)
+            if (move == arr[r][c + 3] && arr[r][c + 1] != move && arr[r][c + 1] == arr[r][c + 2] & arr[r][c + 1] != 0){
+                arr[r][c + 1] = arr[r][c + 2] = 0;
+                return (true);
+            }
+        //left
+        if (c > 3)
+            if (move == arr[r][c - 3] && arr[r][c - 1] != move && arr[r][c - 1] == arr[r][c - 2] & arr[r][c - 1] != 0){
+                arr[r][c - 1] = arr[r][c - 2] = 0;
+                return (true);
+            }
+        //diag down left
+        if (r < 16 && c > 3)
+            if (move == arr[r + 3][c - 3] && arr[r + 1][c - 1] != move && arr[r + 1][c - 1] == arr[r + 2][c - 2] & arr[r + 1][c - 1] != 0){
+                arr[r + 1][c - 1] = arr[r + 2][c - 2] = 0;
+                return (true);
+            }
+        //diag down right
+        if (r < 16 && c < 16)
+            if (move == arr[r + 3][c + 3] && arr[r + 1][c + 1] != move && arr[r + 1][c + 1] == arr[r + 2][c + 2] & arr[r + 1][c + 1] != 0){
+                arr[r + 1][c + 1] = arr[r + 2][c + 2] = 0;
+                return (true);
+            }
+        //diag up right
+        if (c < 16 && r > 3)
+            if (move == arr[r - 3][c + 3] && arr[r - 1][c + 1] != move && arr[r - 1][c + 1] == arr[r - 2][c + 2] & arr[r - 1][c + 1] != 0){
+                arr[r - 1][c + 1] = arr[r - 2][c + 2] = 0;
+                return (true);
+            }
+        //diag up left
+        if (c > 3 && r > 3)
+            if (move == arr[r - 3][c - 3] && arr[r - 1][c - 1] != move && arr[r - 1][c - 1] == arr[r - 2][c - 2] & arr[r - 1][c - 1] != 0){
+                arr[r - 1][c - 1] = arr[r - 2][c - 2] = 0;
+                return (true);
+            }
+        return (false);
+    }
+}
