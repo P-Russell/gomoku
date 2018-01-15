677bd507d88eada86a8566440e9ab529032f984f
P-Russell <pdrussell011@gmail.com>
2018-01-15 14:45:44 +0200

    cleared repo

--- src/Node.java
+++
@@ -1,8 +0,0 @@
-public class Node
-{
-/*
-class inherits from heuristic
-constructor take board configuration and x  and y coordinate and player number - creates a board where player has placed piece
-saves x, y lastPlayedPlayer and board and heuristic value.
- */
-}
