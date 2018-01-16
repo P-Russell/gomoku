06317e9a3b1a532322f0b121fc8d3a4977a636b7
P-Russell <pdrussell011@gmail.com>
2018-01-16 08:28:09 +0200

    started work on expand board

--- src/ExpandBoard.java
+++ src/ExpandBoard.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class ExpandBoard {
+    public ArrayList<Board> expand(Board board) {
+        ArrayList<Board> expanded = new ArrayList<Board>();
+        return expanded;
+    }
+
+    private ArrayList<Move> createOptions(Board board) {
+        ArrayList<Move> moves = new ArrayList<Move>();
+
+        for(int y = 0; y < board.getBoard().length; y++)
+            for(int x = 0; x < bo)
+
+
+
+        return moves;
+    }
+
 }
