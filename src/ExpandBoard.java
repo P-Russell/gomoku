b7f9625042b347d7fb365c5efef1971acf376c75
P-Russell <pdrussell011@gmail.com>
2018-01-16 12:07:20 +0200

    refining expand

--- src/ExpandBoard.java
+++ src/ExpandBoard.java
@@ -5,10 +5,26 @@ public class ExpandBoard {
 
     public static ArrayList<Board> expand(Board board) {
         ArrayList<Board> expanded = new ArrayList<>();
+        int[][] moves = new int[board.getBoard().length][board.getBoard().length];
         CheckMove validator = new CheckMove();
+        int stepFromPlaced = 2;
         for(int y = 0; y < board.getBoard().length; y++)
             for(int x = 0; x < board.getBoard().length; x++) {
-                if (board.getBoard()[y][x] == 0) {
+                if (board.getBoard()[y][x] != board.getLastPlayed()) {
+                    for(int i = stepFromPlaced; i > 0; i--)
+                        if(!(i + x > board.getBoard().length) && moves) {
+
+                        }
+
+
+
+
+
+
+
+
+
+
                     Board node = new Board(board);
                     if (validator.isCaptureMove(node, y, x)) {
                         validator.capturePieces(node, y, x);
--- src/Main.java
+++ src/Main.java
@@ -35,7 +35,7 @@ public class Main {
         for(int i = 0; i < expanded.size(); i++)
             TerminalGame.printBoard(expanded.get(i).getBoard());
 
-        //TestPlay t = new TestPlay();
-        //t.loop();
+        TestPlay t = new TestPlay();
+        t.loop();
     }
 }
